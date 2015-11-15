d8d49ef5da3e5fd88ec79b993444e8ff05f3d7d5
cenenh <gseunwo@gmail.com>
2015-11-16 01:08:58 +0900

    modified for /test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var test = require('./routes/test');
 var app = express();
 
 // view engine setup
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use('/',express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
+app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
